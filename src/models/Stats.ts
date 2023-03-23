a378748b5f53dd7d4576f9a4192e9ccad3508f79
Łukasz Pawłoś <62241491+lukasz-pawlos@users.noreply.github.com>
2023-03-23 22:12:34 +0100

    issue #1 - add stats model (#17)

---
+++ src/models/Stats.ts
@@ -0,0 +1,24 @@
+export class Stats {
+    games_played: Number;
+    player_id: Number;
+    season: Number;
+    min: String;
+    fgm: Number;
+    fga: Number;
+    fg3m: Number;
+    fg3a: Number;
+    ftm: Number;
+    fta: Number;
+    oreb: Number;
+    dreb: Number;
+    reb: Number;
+    ast: Number;
+    stl: Number;
+    blk: Number;
+    turnover: Number;
+    pf: Number;
+    pts: Number;
+    fg_pct: Number;
+    fg3_pct: Number;
+    ft_pct: Number;
+}
