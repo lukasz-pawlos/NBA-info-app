8e9ddb8679a0398107fff9c029095687ef5f9f35
lukasz-pawlos <252925@student.pwr.edu.pl>
2023-06-18 17:41:59 +0200

    issue #28 - homa page

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,15 @@
-import React , { useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { PlayerService } from './services/PlayerService';
-import { DataFromApi } from './models/DataFromApi';
-import { StatsService } from './services/StatsService';
-import { StatsQuery } from './models/StatsQuery';
-import { Stat } from './models/Stat';
-import { TeamService } from './services/TeamService';
-import { Team } from './models/Team';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App() {
 
-  const [serviceData, setServiceData] = useState<Team | undefined>(undefined);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const fetchedData = await TeamService.getTeamById(14);
-        fetchedData !== undefined ? setServiceData(fetchedData) : console.log("No data")
-      } catch (error) {
-        // Handle error
-      }
-    };
-
-    getData();
-  }, []);
-
-  function add() {
-    //@ts-ignore
-    console.log(serviceData)
-    TeamService.addTeamToFavList(serviceData?.id)
-  }
-
-  function rmv() {
-    //@ts-ignore
-    TeamService.rmvTeamfromFavList(serviceData.id)
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <button onClick={add}>
-        Button add
-      </button>
-      <button onClick={rmv}>
-        Button rmv
-      </button>
+      <Routes>
+        <Route path="/" element={ <Home/>}> </Route>
+        <Route path="/players" element={ <Home/>}> </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/CarouselLogos.tsx
@@ -0,0 +1,173 @@
+import { Link } from "react-router-dom";
+
+const CarouselLogos = () => {
+
+    return (
+        <div className="container">
+            <h1 className="text-center py-3">NBA TEAMS</h1>
+            <div id="carouselExampleSlidesOnly" className="row">
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-76ers"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-bucks"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-bulls"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-cavaliers"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-celtics"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-clippers"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-grizzlies"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-hawks"></div>
+                    </Link>
+                </div>
+
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-heat"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-hornets"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-jazz"></div>
+                    </Link>
+                </div>
+
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-kings"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-knicks"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-lakers"></div>
+                    </Link>
+                </div>
+
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-magic"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-mavericks"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-nets"></div>
+                    </Link>
+                </div>
+
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-nuggets"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-pacers"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-pelicans"></div>
+                    </Link>
+                </div>
+
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-pistons"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-raptors"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-rockets"></div>
+                    </Link>
+                </div>
+
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-spurs"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-suns"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-thunder"></div>
+                    </Link>
+                </div>
+
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-timberwolves"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-trail-blazers"></div>
+                    </Link>
+                </div>
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-warriors"></div>
+                    </Link>
+                </div>
+
+                <div className="col">
+                    <Link to="/">
+                        <div className="logo sprite-wizards"></div>
+                    </Link>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default CarouselLogos;
